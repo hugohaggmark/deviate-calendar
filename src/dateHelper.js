f23219bfc89db6a257e8b8605bdb381af1a9a74c
Hugo <hugo.haggmark@gmail.com>
2016-10-18 14:42:54 +0200

    checking for existence

--- src/dateHelper.js
+++ src/dateHelper.js
@@ -19,14 +19,26 @@ export const getformattedDate = date => {
   return formatDate(new Date(date).getFullYear(), new Date(date).getMonth(), new Date(date).getDate())
 }
 export const isWeekEnd = date => {
+  if(!date)
+    return false
+
   return date.getDay() === 0 || date.getDay() === 6
 }
 export const isDateInArray = (date, array) => {
+  if(!array)
+    return false
+
   return array.filter(d => getformattedDate(d) === getformattedDate(date)).length > 0
 }
 export const removeDateFromArray = (date, array) => {
+  if(!array)
+    return false
+
   return array.filter(d => getformattedDate(d) !== getformattedDate(date))
 }
 export const getDatesInArrayForThisYearMonth = (year, month, array) => {
+  if(!array)
+    return false
+
   return array.filter(day => new Date(day).getFullYear() === year && new Date(day).getMonth() === month)
 }
