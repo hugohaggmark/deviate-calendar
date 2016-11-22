6d6d633f8c6a17c5a6df2a4e07d7de1167dc0674
Hugo <hugo.haggmark@gmail.com>
2016-11-22 18:25:48 +0100

    added reset buttuon

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ class App extends Component {
   render() {
     return (
       <div className="container">
+        <a role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+          <h3 className="glyphicon glyphicon-menu-hamburger"></h3>
+        </a>
+        <div className="collapse" id="collapseExample">
+          <div className="well">
+            <a href="#" className="btn btn-danger" onClick={() => localStorage.removeItem('redux')}>Nollställ</a>
+          </div>
+        </div>
         {/* <a href="#" className="btn btn-default" onClick={() => localStorage.removeItem('redux')}>Clear state...</a> */}
         <Info/>
         <Calendar />
