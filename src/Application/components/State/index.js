ad077473c1a12cd0cc4c045a7c30568457a1efdd
Hugo <hugo.haggmark@gmail.com>
2016-12-07 15:38:48 +0100

    fixed state

--- src/Application/components/State/index.js
+++ src/Application/components/State/index.js
@@ -3,19 +3,21 @@ import Modal from '../Modal'
 
 export default class State extends Component {
   render() {
-    const redux = localStorage.getItem("redux")
+    const redux = JSON.parse(localStorage.getItem("redux"))
     return(
       <div className="container">
         <div className="row">
           <div className="well">
             <button type="button" className="btn btn-danger" data-toggle="modal" data-target="#myModal">
-              Nollställ tillståndet
+              Nollställ data
             </button>
-            <Modal title="Nollställ tillståndet" body="Nollställ kommer att rensa allt du har matat in någonsin" okLabel="Nollställ tillståndet" okAction={() => localStorage.removeItem('redux')}/>
+            <Modal title="Nollställ data" body="Nollställ kommer att rensa allt data du har matat in någonsin" okLabel="Nollställ data" okAction={() => localStorage.removeItem('redux')}/>
           </div>
-          <h3>Nuvarande tillstånd</h3>
+          <h3>Nuvarande data</h3>
           <div className="well well-lg">
-            {redux}
+            <pre>
+              {JSON.stringify(redux, null, 2)}
+            </pre>
           </div>
         </div>
       </div>
