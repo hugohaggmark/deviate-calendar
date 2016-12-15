8ed0cd7f51cb8ccb2b2c8dcfbf7045252495b479
Hugo <hugo.haggmark@gmail.com>
2016-12-15 12:53:59 +0100

    some margins on device

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
         <div className="row">
           <Calendar />
           <div className="row">
-            <div className="col-xs-10 col-xs-offset-1 col-sm-7 col-sm-offset-0">
+            <div className="col-xs-12 col-sm-7 col-sm-offset-0">
               <div className="box-header pointer" onClick={this.toggleInfo}>
                 {!showInfo && <span>Visa personuppgifter</span>}
                 {!showInfo && <span className="pull-right"><i className="glyphicon glyphicon-chevron-right"></i></span>}
@@ -51,7 +51,7 @@ class App extends Component {
             </div>
           </div>
           <div className="row">
-            <div className="col-xs-10 col-xs-offset-1 col-sm-7 col-sm-offset-0">
+            <div className="col-xs-12 col-sm-7 col-sm-offset-0">
               <div className="box-header pointer" onClick={this.toggleReport}>
                 {!showReport && <span>Visa tidrapport</span>}
                 {!showReport && <span className="pull-right"><i className="glyphicon glyphicon-chevron-right"></i></span>}
